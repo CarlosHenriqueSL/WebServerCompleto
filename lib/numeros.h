3ec1bfaf144b87e84125f037f8b76a93eb234b2f
Carlos Henrique <luiscarloslopes597@gmail.com>
2025-05-20 15:01:39 -0300

    Enviando arquivos ao repositório.

---
+++ lib/numeros.h
@@ -0,0 +1,14 @@
+#ifndef NUMEROS_H
+#define NUMEROS_H
+
+#define NUM_PIXELS 25
+
+extern double zeroPorcento[NUM_PIXELS];
+extern double vintePorcento[NUM_PIXELS];
+extern double quarentaPorcento[NUM_PIXELS];
+extern double sessentaPorcento[NUM_PIXELS];
+extern double oitentaPorcento[NUM_PIXELS];
+extern double cemPorcento[NUM_PIXELS];
+
+
+#endif
